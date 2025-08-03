d780dcc9cb703930ac7599e58b28f14f3f8a1193
SurpassHR <hu.rui0530@gmail.com>
2025-08-03 16:54:09 +0800

    fix: 1. quit.bat issue at killing frontend port process; 2. add triton whl file to track.

--- quit.bat
+++ quit.bat
@@ -2,10 +2,16 @@
 @echo off
 setlocal enabledelayedexpansion
 
-set pid=65535
+set "pid=65535"
 @REM Find the ComfyUI process ID through netstat
 for /f "tokens=5" %%i in ('netstat -ano ^| findstr :55555') do (
+    REM Count PID number, and kill the one that count max
+    set "pid=65535"
+    for /f "tokens=1" %%j in ('tasklist /FI "PID eq %%i" ^| findstr /I "ComfyUI"') do (
+        set "pid=%%i"
+    )
     set "pid=%%i"
 )
 @REM echo Found ComfyUI process with PID !pid!
+echo killing ComfyUI process with PID !pid!
 taskkill /F /PID !pid! >nul 2>&1
